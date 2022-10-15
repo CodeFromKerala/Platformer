5c136d0db478459c5dcdfc78d4d2b0ea2559ad26
CodeFromKerala <mjnair52@gmail.com>
2022-10-15 14:52:15 +0530

    Made Loading Screen and Player Movements

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ display = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("Platformer")
 clock = pygame.time.Clock()
 
-player_imgs = ["player1.png", "player2.png", "player3.png", "player4.png"]
+player_imgs = ["./gfx/player1.png", "./gfx/player2.png", "./gfx/player3.png", "./gfx/player4.png"]
+btn_imgs = ["./gfx/play_btn.png", "./gfx/", "./gfx/", "./gfx/"]
 enemy_imgs = []
 
 FPS = 5
@@ -26,22 +27,83 @@ class Player(pygame.Rect):
         self.centerx = x
         self.centery = y
         self.image = pygame.image.load(player_imgs[0])
+        self.MoveW = False
+        self.MoveA = False
+        self.MoveS = False
+        self.MoveD = False
     def update(self):
+        if self.MoveW == True:
+            self.centery -= 1
+        if self.MoveS == True:
+            self.centery += 1
+        if self.MoveD == True:
+            self.centerx += 1
+        if self.MoveA == True:
+            self.centerx -= 1
         display.blit(self.image, self)
 
+# Button Class
+
+class Btn(pygame.Rect):
+    def __init__(self, x, y):
+        self.centerx = x
+        self.centery = y
+        self.image = pygame.image.load(btn_imgs[0])
+    def update(self):
+        display.blit(self.image, self)
+    def m_pressed(self):
+        pos = pygame.mouse.get_pos()
+        m_x = pos[0]
+        m_y = pos[1]
+        if (m_x > self.x and m_x < self.x + 100) and (m_y > self.y and m_y < self.y + 50):
+            main()
 # main game loop
-def main():
+def Start():
     run = True
     bg = Background()
+    btn = Btn(330, 400)
     clock.tick(FPS)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                btn.m_pressed()
         display.fill((255, 255, 255))
         bg.update()
+        btn.update()
+        pygame.display.update()
+
+def main():
+    run = True
+    player = Player(50, 50)
+    clock.tick(FPS)
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player.MoveW = True
+                if event.key == pygame.K_s:
+                    player.MoveS = True
+                if event.key == pygame.K_a:
+                    player.MoveA = True
+                if event.key == pygame.K_d:
+                    player.MoveD = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player.MoveW = False
+                if event.key == pygame.K_s:
+                    player.MoveS = False
+                if event.key == pygame.K_a:
+                    player.MoveA = False
+                if event.key == pygame.K_d:
+                    player.MoveD = False
+        display.fill((255, 255, 255))
+        player.update()
         pygame.display.update()
 
 # Checking if file is not imported
 if __name__ == "__main__":
-    main()
+    Start()
